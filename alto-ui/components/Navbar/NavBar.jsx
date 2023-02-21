db646bd7143e3e010decbdc85e361d9cbd26c1a6
Khadeeejah <wuraola81@gmail.com>
2023-02-21 07:33:04 +0100

    update-marketplace and ui

---
+++ alto-ui/components/Navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Style from './NavBar.module.css'
+import { MdNotifications } from 'react-icons/md'
+import { BsSearch } from 'react-icons/bs'
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg'
+import { Discover, HelpCenter, Notification, Profile, SideBaar } from './index'
+import { Button } from '../componentindex'
+const NavBar = () => {
+  return <div>Navbar</div>
+}
+
+export default NavBar
--- alto-ui/pages/_app.js
+++ alto-ui/pages/_app.js
@@ -1,5 +1,130 @@
-import '../styles/globals.css'
+import React from 'react'
+import { ethers } from 'ethers'
+const NFT_ADDRESS = '0xB9e76f90838CaaFEcb601754fbd2B112D513869a' // NFT contract address
+const AUCTIONMANAGER_ADDRESS = '0x0cc272d5EeE3997834A177921135A962d2d00b71' // AuctionManager contract address
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      auctions: [], // Auctions to display
+      newAuction: {
+        // newAuction is a state variable for the form
+        startPrice: null,
+        endTime: null,
+        tokenId: null,
+        minIncrement: null,
+        directBuyPrice: null,
+      },
+      myItems: [],
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div class="jumbotron d-flex align-items-center">
+          <div class="container">
+            <div class="auctions row"></div>
+            <form>
+              <div class="mb-3">
+                <label for="startprice" class="form-label">
+                  Start Price
+                </label>
+                <input
+                  value={this.state.newAuction.startPrice}
+                  onChange={(e) =>
+                    this.setState({
+                      newAuction: {
+                        ...this.state.newAuction,
+                        startPrice: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                  type="number"
+                  class="form-control"
+                  id="startprice"
+                />
+                <label for="startprice" class="form-label">
+                  Token Id
+                </label>
+                <input
+                  value={this.state.newAuction.tokenId}
+                  onChange={(e) =>
+                    this.setState({
+                      newAuction: {
+                        ...this.state.newAuction,
+                        tokenId: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                  type="number"
+                  class="form-control"
+                  id="startprice"
+                />
+                <label class="form-label">Minimum Increment</label>
+                <input
+                  value={this.state.newAuction.minIncrement}
+                  onChange={(e) =>
+                    this.setState({
+                      newAuction: {
+                        ...this.state.newAuction,
+                        minIncrement: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                  type="number"
+                  class="form-control"
+                />
+                <label class="form-label">Direct Buy Price</label>
+                <input
+                  value={this.state.newAuction.directBuyPrice}
+                  onChange={(e) =>
+                    this.setState({
+                      newAuction: {
+                        ...this.state.newAuction,
+                        directBuyPrice: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                  type="number"
+                  class="form-control"
+                />
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+                <label class="form-label">Duration In Minutes</label>
+                <input
+                  value={this.state.newAuction.endTime}
+                  onChange={(e) =>
+                    this.setState({
+                      newAuction: {
+                        ...this.state.newAuction,
+                        endTime: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                  type="number"
+                  class="form-control"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
 
-export default MyApp
+        <div class="container">
+          <button
+            type="button"
+            onClick={() => console.log(this.state.newAuction)}
+            class="btn btn-primary"
+          >
+            Create Auction
+          </button>
+          <button class="btn btn-fanger">Mint NFT</button>
+          <p>
+            Your items
+            <br />
+            {(this.state.myItems || ['']).map((x) => `id: ${x} `) || ''}
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+export default App
